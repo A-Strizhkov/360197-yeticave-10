5886863e307b08e11fc14d1378b7bfab5542f20c
Andrew <Strizhkov22@yandex.ru>
2019-08-27 20:01:57 +0300

    Написал запросы SQL Модуль 4-2

--- queries.sql
+++ queries.sql
@@ -8,40 +8,39 @@ VALUES ('Андрей','andrey@yandex.ru', 'vrtyuuu', '@yanti'),('Витя','pet
 INSERT INTO  lots (title, image_link, start_price, date_end, author_id, category_id)
 VALUES ('2014 Rossignol District Snowboard', 'img/lot-1.jpg', '10999', '2019-10-19', '139', '1'),('DC Ply Mens 2016/2017 Snowboard', 'img/lot-2.jpg', '159999', '2019-11-20', '140', '2'),('Крепления Union Contact Pro 2015 года размер L/XL', 'img/lot-3.jpg', '8000', '2019-12-20', '143', '3'),('Ботинки для сноуборда DC Mutiny Charocal', 'img/lot-4.jpg', '10999', '2019-12-19', '144', '4'),('Куртка для сноуборда DC Mutiny Charocal', 'img/lot-5.jpg', '7500', '2019-10-19', '141', '5'),('Маска Oakley Canopy', 'img/lot-6.jpg', '5400', '2019-10-19', '142', '6');
 
-INSERT INTO  bid (bid)
+INSERT INTO  bid (amount)
 VALUES ('112'),('245'),('356'),('466'),('665.6'),('6666.8');
 
 Связь таблицы лот с автором, победителем и категорией
-SELECT lot.id, title, discription, link, start_price, dt_end, name_user,  FROM lot l
-INNER JOIN categories c ON c.id_lot = l.id
-INNER JOIN users u ON l.id_users = u.id
-INNER JOIN u ON l.winner = u.id
-INNER JOIN categories c ON l.id_cat = c.id ;
+SELECT l.id, l.title, l.description, l.image_link, l.start_price, l.date_end, u.name, u_w.name  FROM lots l
+JOIN users u ON  l.author_id = u.name
+LEFT JOIN users u_w ON l.winner_id = u_w.name
+INNER JOIN categories c ON l.category_id = c.id ;
 
-Связь таблицы ставки с лотом и автором.
-SELECT bid.id, dt_start, price,   FROM bid b
-INNER JOIN lot l ON b.id_lot = l.id
-INNER JOIN users u ON b.id_users = u.id;
+Связь таблицы ставок с лотом и автором.
+SELECT b.id, b.date_start, b.amount, u.name FROM bid b
+INNER JOIN lots l ON b.lot_id = l.id
+INNER JOIN users u ON b.user_id = u.name;
 
 Связи таблицы автора с лотом и ставкой.
-SELECT users.id, l.id, id_bid  FROM users u
-INNER JOIN lot l ON u.create_lot = l.id
-INNER JOIN bid b ON u.id_bid = b.bid;
+SELECT u.id, l.id, b.amount  FROM users u
+INNER JOIN lots l ON u.lot_id = l.id
+INNER JOIN bid b ON u.bid_id = b.id;
 
 Получаем категории
-SELECT id FROM categories;
+SELECT * FROM categories;
 
 Получаем самые новые открытые лоты
-SELECT title, start_price, link, id_cat FROM lot ORDER BY dt_create
-LIMIT 10;
+SELECT title, start_price, image_link, category_id FROM lots ORDER BY created_at
+LIMIT 9;
  
-Показать лот по его id_cat
-SELECT id, id_cat FROM lot;
+Показать лот по его id
+SELECT * FROM lots WHERE id != 0;
 
 Обновляем название лота по его идентификатору
 
-UPDATE lot SET title = 'DC Ply Mens 2016/2017 Snowboard'’
+UPDATE lots SET title = 'DC Ply Mens 2016/2017 Snowboard'
 WHERE id = '1';
 
 Получаем список ставок для лота по его идентификатору с сортировкой по дате.
-SELECT id_lot, bid from bid ORDER BY dt_start DESC;
+SELECT amount, lot_id from bid ORDER BY date_start DESC;
