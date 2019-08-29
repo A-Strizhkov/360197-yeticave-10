b6b55788614516e2a69832b94d5b596110252887
Andrew <Strizhkov22@yandex.ru>
2019-08-29 23:31:09 +0300

    Закончил правки из ТГ Модуль 4-2

--- queries.sql
+++ queries.sql
@@ -15,21 +15,21 @@ INSERT INTO  bid (lot_id, user_id)
 VALUES ('53','139'),('54','140'),('55', '141'),('56','142'),('57', '143'),('58', '144');
 
 Связь таблицы лот с автором, победителем и категорией
-SELECT l.id, l.title, l.description, l.image_link, l.start_price, l.date_end, u.name, u_w.name  FROM lots l
-JOIN users u ON  l.author_id = u.name
-LEFT JOIN users u_w ON l.winner_id = u_w.name
+SELECT l.id, l.title, l.description, l.image_link, l.start_price, l.date_end, u.id, u_w.id  FROM lots l
+JOIN users u ON  l.author_id = u.id
+LEFT JOIN users u_w ON l.winner_id = u_w.id
 INNER JOIN categories c ON l.category_id = c.id ;
 
 Связь таблицы ставок с лотом и автором.
 SELECT b.id, b.date_start, b.amount, u.name FROM bid b
 INNER JOIN lots l ON b.lot_id = l.id
-INNER JOIN users u ON b.user_id = u.name;
+INNER JOIN users u ON b.user_id = u.id;
 
 Получаем категории
 SELECT * FROM categories;
 
 Получаем самые новые открытые лоты
-SELECT title, start_price, image_link, category_id FROM lots ORDER BY created_at
+SELECT title, start_price, image_link, category_id FROM lots ORDER BY created_at DESC
 LIMIT 9;
 
 Показать лот по его id
@@ -42,4 +42,4 @@ WHERE id = '1';
 
 Получаем список ставок для лота по его идентификатору с сортировкой по дате.
 
-SELECT lot_id FROM bid WHERE id = 1 ORDER BY date_start ASC;
+SELECT lot_id, amount, user_id FROM bid WHERE lot_id = 1 ORDER BY date_start ASC;
