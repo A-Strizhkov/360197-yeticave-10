ff07fdbcaf931b0b6c3c098642346d8a276318a4
Andrew <Strizhkov22@yandex.ru>
2019-08-31 16:45:58 +0300

    Закончил правки из ТГ Модуль 5-1

---
+++ config/db.php
@@ -0,0 +1,7 @@
+<?php
+$db = [
+    'host' => 'localhost',
+    'user' => 'root',
+    'password' => '12345',
+    'database' => 'yeticave'
+];
--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <?php
 require_once('functions.php');
 require_once('templates/date.php');
+require_once 'init.php';
+if (!$link) {
+    $error = mysqli_connect_error();
+    $content = include_template('error.php', ['error' => $error]);
+}
+else {
+    $sql = 'SELECT `id`, `name` FROM categories';
+    $result = mysqli_query($link, $sql);
+    if ($result) {
+        $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    }
+    else {
+        $error = mysqli_error($link);
+        $content = include_template('error.php', ['error' => $error]);
+    }
+}
+
+
 $page_content = include_template('main.php', ['categories' => $categories,'lots' => $lots]);
 $layout_content = include_template('layout.php', [
     'content' => $page_content,
@@ -11,6 +29,5 @@ $layout_content = include_template('layout.php', [
 ]);
 print ($layout_content);
 ?>
-
 
 
